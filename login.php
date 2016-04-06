8f78597abdcc189886e6140f1e6b3e1ec07ffe1b
Daniel Alzugaray <dalzuga@gmail.com>
2016-04-06 15:15:39 -0700

    saving

--- login.php
+++ login.php
@@ -53,7 +53,7 @@
          ?>
       </div> <!-- /container -->
 
-      <div class = "container">
+      <div class = "container form-boxes">
 
          <form class = "form-signin" role = "form"
             action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);
@@ -69,6 +69,23 @@
          </form>
 
       </div>
+
+      <form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+        <fieldset >
+          <legend>Login</legend>
+          <input type='hidden' name='submitted' id='submitted' value='1'/>
+
+          <label for='username' >UserName*:</label>
+          <input type='text' name='username' id='username'  maxlength="50" />
+
+          <label for='password' >Password*:</label>
+          <input type='password' name='password' id='password' maxlength="50" />
+
+          <input type='submit' name='Submit' value='Submit' />
+
+        </fieldset>
+      </form>
+
     </div>
   </body>
 
